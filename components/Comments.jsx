a993d062482b84033f451bf2e92a70c1dca982f7
Rakeshbora007 <www.rakeshbora365@gmail.com>
2024-01-07 13:20:39 +0530

    up

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable multiline-ternary */
 'use client'
 import { useSession } from 'next-auth/react'
 import React, { useState } from 'react'
@@ -5,6 +6,7 @@ import useSWR from 'swr'
 import Reply from './Reply'
 import Image from 'next/image'
 import Dateformat from './Dateformat'
+import Link from 'next/link'
 const api = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 export const postComment = async (endpoint, data) => {
   try {
@@ -64,66 +66,68 @@ const Comments = ({ placeholder, buttons, id }) => {
       <div className="gap-7 h-auto  flex flex-col w-full">
         {data?.length === 0
           ? (
-          <div>No comments yet</div>
+            <div>No comments yet</div>
             )
           : (
-          <>
-            {data?.map((e) => (
-              <div key={e._id}>
-                <div className="flex my-5 w-full  h-auto " >
-                  <div className="flex justify-center h-auto   w-full items-center flex-col ">
-                    <div className="flex flex-col gap-[20px] items-center w-full justify-start ">
-                      <div className="flex  gap-[30px] items-center w-full justify-start ">
-                        <div className="h-[60px] relative w-[60px] ">
-                          <Image alt='dataimage' src={e.image} fill className='rounded-full' />
-                        </div>
-                        <div className="flex-col flex  gap-4 w-full h-auto ">
-                          <div className="flex items-center h-3  justify-between">
-                            <span className="text-[20px] font-semibold">
-                              <span>{e.name}</span>
-                            </span>
-                            <span className="text-[16px] max-sm:text-[12px] text-[#878787]">
-                              <Dateformat createdAt={e.createdAt} />
-                            </span>
+            <>
+              {data?.map((e) => (
+                <div key={e._id}>
+                  <div className="flex my-5 w-full  h-auto " >
+                    <div className="flex justify-center h-auto   w-full items-center flex-col ">
+                      <div className="flex flex-col gap-[20px] items-center w-full justify-start ">
+                        <div className="flex  gap-[30px] items-center w-full justify-start ">
+                          <div className="h-[60px] relative w-[60px] ">
+                            <Image alt='dataimage' src={e.image} fill className='rounded-full' />
                           </div>
-                          <div className="w-3/5">
-                            <p className="text-[16px] capitalize">
-                              {e.comment}
-                            </p>
+                          <div className="flex-col flex  gap-4 w-full h-auto ">
+                            <div className="flex items-center h-3  justify-between">
+                              <span className="text-[20px] font-semibold">
+                                <span>{e.name}</span>
+                              </span>
+                              <span className="text-[16px] max-sm:text-[12px] text-[#878787]">
+                                <Dateformat createdAt={e.createdAt} />
+                              </span>
+                            </div>
+                            <div className="w-3/5">
+                              <p className="text-[16px] capitalize">
+                                {e.comment}
+                              </p>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="pl-24 w-full">
-                        <Reply commentmutate={mutate} idmain={id} replyID={e._id} likes={e.likecomments} display={true} likefor={'comments'} />
+                        <div className="pl-24 w-full">
+                          <Reply commentmutate={mutate} idmain={id} replyID={e._id} likes={e.likecomments} display={true} likefor={'comments'} />
+                        </div>
                       </div>
                     </div>
+                    <hr />
                   </div>
                   <hr />
                 </div>
-                <hr />
-              </div>
-            ))}
-          </>
+              ))}
+            </>
             )}
       </div>
-      <div className="mt-[160px] max-md:mt-4 gap-7 flex flex-col">
-        <div className="flex w-full  justify-center items-center  h-[70px]">
-          <span className=" text-[24px]  max-md:text-[17px] font-bold w-[280px]">
-            Leave a Comment
-          </span>
-          <hr className="w-full" />
-        </div>
-        <form onSubmit={handleCommentSubmit} className="p-2 flex w-full gap-4">
-          <input
-            type="text"
-            className="w-full h-[227px]  rounded-md pl-[52px] bg-[#e5e5e5] placeholder:text-[#585858] placeholder:text-[14px]"
-            placeholder={placeholder}
-            value={comment}
-            onChange={handleComments}
-          />
-        </form>
-        <buttons type="submit" className="bg-[#1D3208] flex w-[186px] items-center justify-center text-[#AFE67F] h-[58px] rounded-lg">Submit</buttons>
-        {/* <div className='text-[18px] mt-[20px] flex gap-[2%]'>
+      {session?.status === 'unauthenticated' ? <Link href='/login' className='bg-[#1D3208] mt-[20px] flex w-[186px] rounded-lg cursor-pointer items-center justify-center text-[#AFE67F] h-[48px] rounded-lg"'>Login for comment</Link>
+        : <>
+          <div className="mt-[160px] max-md:mt-4 gap-7 flex flex-col">
+            <div className="flex w-full  justify-center items-center  h-[70px]">
+              <span className=" text-[24px]  max-md:text-[17px] font-bold w-[280px]">
+                Leave a Comment
+              </span>
+              <hr className="w-full" />
+            </div>
+            <form onSubmit={handleCommentSubmit} className="p-2 flex w-full gap-4">
+              <input
+                type="text"
+                className="w-full h-[227px]  rounded-md pl-[52px] bg-[#e5e5e5] placeholder:text-[#585858] placeholder:text-[14px]"
+                placeholder={placeholder}
+                value={comment}
+                onChange={handleComments}
+              />
+            </form>
+            <buttons type="submit" className="bg-[#1D3208] flex w-[186px] items-center justify-center text-[#AFE67F] h-[58px] rounded-lg">Submit</buttons>
+            {/* <div className='text-[18px] mt-[20px] flex gap-[2%]'>
           <span>Food</span>
           <span>Style</span>
           <span>Travel</span>
@@ -131,8 +135,9 @@ const Comments = ({ placeholder, buttons, id }) => {
           <span>Travel</span>
           <span>Health</span>
         </div> */}
-      </div>
-
+          </div>
+        </>
+      }
     </div>
   )
 }
